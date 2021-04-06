7f8eae7d70365237b5f0162d8d3d1aa93a4e0639
Yazan <79808950+yazandk@users.noreply.github.com>
2021-04-06 19:23:34 +0200

    Added files

---
+++ KRNL Key Bypasser/KRNL Key Bypasser/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KRNL_Key_Bypasser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            System.Diagnostics.Process.Start("https://linkvertise.com/48193/krnlkey?o=sharing");
+
+
+            string message = "KRNL Key Has Been Bypassed!";
+            string title = "KRNL Key Bypasser";
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            DialogResult result = MessageBox.Show(message, title, buttons, MessageBoxIcon.Asterisk);
+            if (result == DialogResult.Abort)
+            {
+                
+            }
+
+            
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+            protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case 0x84:
+                    base.WndProc(ref m);
+                    if ((int)m.Result == 0x1)
+                        m.Result = (IntPtr)0x2;
+                    return;
+            }
+
+            base.WndProc(ref m);
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://linkvertise.com/48193/krnlc4?o=sharing");
+        }
+    }
+    
+}
